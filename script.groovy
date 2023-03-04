13e3d1e4ebcf9d9216b24940a3a609a96cd840ef
Abbabus79 <110320410+Abbabus79@users.noreply.github.com>
2023-03-04 14:39:28 -0700

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,25 @@
+def buildApp() {
+    echo "building the application..."
+    sh 'npm install'
+    sh 'npm build'
+} 
+
+def testApp() {
+    echo 'testing the application'
+}
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-my-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t papis84/my-repo:njsa-1.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push papis84/my-repo:njsa-1.0'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+    echo "deploying version ${params.VERSION}"
+} 
+
+return this
